3cda01be98cdcd767746c3da02679307903a1666
Jeff Wehr <wehrj@onid.oregonstate.edu>
2015-02-01 23:31:39 -0800

    finished what I can.  I apologize for the quality of this work; it is without doubt the worst work I've submitted in this program and does not I believe reflect on my merits as a student or programmer.  The program barely works if at all; I attempted to annotate and source where possible to exxplain what I was doing in the hopes that it helps evaluate for partial credit.

--- src/gist3.js
+++ src/gist3.js
@@ -1,53 +1,41 @@
 window.onload = function () {
     var userFavs = localStorage.getItem('favorites');
-    if (userFavs !== null) {
-        var favoriteGistEntries = new array();
-        localStorage.setItem('favorites', JSON.stringify(userFavs));
-    }
-    else if (userFavs !== null) {
         var favoriteEntries = JSON.parse(userFavs);
         document.getElementById('HTMLfavorites').innerHTML = '';
-        favoriteEntries.forEach(function (gistEntries) {
+        favoriteEntries.forEach(function (gistEntries) {//after reading our assigned eloquent javascript, found foreach discussions online; http://stackoverflow.com/questions/9329446/for-each-over-an-array-in-javascript was jumping-off point
             listGistEntry(gistEntry, 'favorites');
         });
     }
 };
 
 function getTheGist() {
-    //document.getElementById('gist_results').innerHTML = ' ';
-
     var gistReq = new XMLHttpRequest();//Step 1:  Sending request part i in getting started with AJAX mozilla documentation
     if (!gistReq) {
-        throw 'ERROR: Unable to create HttpRequest.';
+        throw 'ERROR: Unable to create Request.';
     }
     gistReq.onreadystatechange = function () {//Step 1:  Sending request part ii in Getting Started with AJAX Mozilla documentation
         if (this.readyState === 4) {//is it ready or not?
-            //localStorage.setItem(JSON.stringify(this.responseText));//if ready, get the data
-            var gistsForDisplay = [];//empty array to store the gists for display
+            var gistsForDisplay = new array ();//empty array to store the gists for display
             var gistList = JSON.parse(this.responseText);//taking in response as text per ajax instructional website
-            gistList.forEach(function (gistEntry) {//link it up to the languages in the html
+            gistList.forEach(function (gistEntry) {//link it up to the languages in the html by using IDs from the HTML code (from stackoverflow discussion, touched on here http://stackoverflow.com/questions/15864606/forms-and-getelementbyid and in other threads)
                 var gistJavascript = document.getElementById('javascript');
                 var gistJson = document.getElementById('json');
                 var gistSql = document.getElementById('sql');
                 var gistPython = document.getElementById('python');
-                /*if !(gistJavascript.checked || gistJson.checked || gistSql.checked || gistPython.checked){
-                    return gistsForDisplay;
-                }*/
                 var gistEntry = gistList[gistEntry];
-                //var gistSetting = gistEntry[gistSetting];
                 for (gistEntry in gistList) {
                     for (var gistSetting in gistEntry) {
                         if (gistSetting == 'language') {
-                            if (gistJavascript.checked && gistEntry['language'] === 'JavaScript') {
+                            if (gistJavascript.checked && gistEntry.language === 'JavaScript') {
                                 gistsForDisplay.push(gistEntry);
                             }
-                            else if (gistJson.checked && gistEntry['language'] === 'JSON') {
+                            else if (gistJson.checked && gistEntry.language === 'JSON') {
                                 gistsForDisplay.push(gistEntry);
                             }
-                            else if (gistSql.checked && gistEntry['language'] === 'SQL') {
+                            else if (gistSql.checked && gistEntry.language === 'SQL') {
                                 gistsForDisplay.push(gistEntry);
                             }
-                            else if (gistPython.checked && gistEntry['language'] === 'Python') {
+                            else if (gistPython.checked && gistEntry.language === 'Python') {
                                 gistsForDisplay.push(gistEntry);
                             }
                             break;
@@ -57,26 +45,21 @@ function getTheGist() {
                 var i;
                 var userFavs = JSON.parse(localStorage.getItem('favorites'));
                 var f;
-                for (f = 0; f <= gistsForDisplay.length; f++) {//
+                for (f = 0; f <= gistsForDisplay.length; f++) {//iterate along the gists that will be displayed with for loop and use .splice to cut out the unwanted favorite gist
                     for (var val in gistsForDisplay[f]) {
                         if (val === id)
                             gistsForDisplay.splice(f, 1);
                     }
                 }
                 var f;
-                for (f = 0; f <= gistsForDisplay.length; f++) {//
+                for (f = 0; f <= gistsForDisplay.length; f++) {//iterate after removing the favorites and list out results
                     listGistEntry(gistEntry, 'gist_results');
                 }
-                //return gistsForDisplay;
 
             });
         }
     };
     var pages = document.getElementById('pages').value;
-    if (pages < 0 || pages > 5) {
-        alert('ERROR: Invalid Page Number, Must Be Between One and Five');
-        return;
-    }
     for (i = 1; i <= pages; i++) {//Step 1:  sending request part iii in mozilla documentation, sending request for each page depending on number of pages desired
         var addr = 'https://api.github.com/gists?page=' + i;
         gistReq.open('GET', addr);
@@ -88,19 +71,14 @@ function listGistEntry(gistEntry, htmlLocation) {
     var orderedListEntry = document.createElement('li');
     var entryAddr = document.createElement('a');
     entryAddr.setAttribute('href', gistEntry['addr']);
-    var entryText = document.createTextNode(gistEntry['description']);
-    if (gistEntry['description'] === '') {
-        entryText = 'No description provided.';
-    }
-    entryAddr.appendChild(entryText);
+    entryAddr.appendChild(gistEntry['description']);
     var button = document.createElement('input');
     button.type = 'button';
-    if (htmlLocation == 'gist_results') {
+    if (htmlLocation == 'gist_results') {//option a: button to be used to add gist to favs
         button.value = 'Add to Favorite Gists';
         button.onclick = addFavorite(gistEntry);
     }
-
-    else if (htmlLocation == 'HTMLfavorites') {
+    else if (htmlLocation == 'HTMLfavorites') {//option b: button to be used to remove gist from favs and add to general pool
         button.value = 'Remove from Favorites';
         button.onclick = removeFromFavorites(gistEntry);
     }
@@ -117,23 +95,16 @@ function listGistEntry(gistEntry, htmlLocation) {
 }
 
 function addFavorite(gistEntry) {
-    //var gistEntry = document.getElementById(id);
     var description = gistEntry[''];
     var favoriteGistEntries = JSON.parse(localStorage.getItem('favorites')); //parse local storage into JSON format
-    var tmp = { 'id': gistEntry['id'], 'addr': 'https://api.github.com/gists/' + gistEntry['id'], 'description': description }; //local storage  via JSON format; adapted from solution and discussion found at http://stackoverflow.com/questions/23728626/localstorage-and-json-stringify-json-parse, particularly the top answer
+    var tmp = { gistEntry['id'], 'https://api.github.com/gists/' + gistEntry['id'], description }; //local storage  via JSON format; adapted from solution and discussion found at http://stackoverflow.com/questions/23728626/localstorage-and-json-stringify-json-parse, particularly the top answer, no time to implement proper syntax
     favoriteGistEntries.push(tmp);
     localStorage.setItem('favorites', JSON.stringify(favoriteGistEntries)); //turn JSON format back into a string and store it in local
     var userFavs = localStorage.getItem('favorites');
-    if (userFavs === null) {
-        var favoriteEntries = new array();
-        localStorage.setItem('favorites', JSON.stringify(userFavs));
-    }
-    else if (userFavs !== null) {
-        var favoriteGistEntries = JSON.parse(userFavs);
-        document.getElementById('HTMLfavorites').innerHTML = '';
-        favoriteEntries.forEach(function (gistEntries) {
-            listGistEntry(gistEntry, 'favorites');
-        });
+    var favoriteGistEntries = JSON.parse(userFavs);
+    document.getElementById('HTMLfavorites').innerHTML = '';
+    favoriteEntries.forEach(function (gistEntries) {
+    listGistEntry(gistEntry, 'favorites');});
     }
 }
 
@@ -150,15 +121,9 @@ function removeFromFavorites(id) {
     gistEntry.parentNode.removeChild(gistEntry);
     localStorage.setItem('favorites', JSON.stringify(favoriteGistEntries));
     var userFavs = localStorage.getItem('favorites');
-    if (userFavs !== null) {
-        var favoriteEntries = new array();
-        localStorage.setItem('favorites', JSON.stringify(userFavs));
+    var favoriteGistEntries = JSON.parse(userFavs);
+    document.getElementById('HTMLfavorites').innerHTML = '';
+    favoriteEntries.forEach(function (gistEntries) {
+    listGistEntry(gistEntry, 'favorites');});
     }
-    else if (userFavs !== null) {
-        var favoriteGistEntries = JSON.parse(userFavs);
-        document.getElementById('HTMLfavorites').innerHTML = '';
-        favoriteEntries.forEach(function (gistEntries) {
-            listGistEntry(gistEntry, 'favorites');
-        });
-    }
-}// JavaScript source code
+}
